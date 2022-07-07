7b485c898ed95502d2f35a4f62c85767723398f0
Ethan Stevenson <Mr.NavaStar@gmail.com>
2022-07-06 18:15:35 -0600

    add a bussin front end

---
+++ router.py
@@ -0,0 +1,23 @@
+import os
+
+from flask import Flask, render_template, Response
+import shopping_list
+from dotenv import load_dotenv
+
+app = Flask("grocy-python", template_folder="web", static_folder="web")
+load_dotenv()
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/api/export_shopping_list")
+def export_shopping_list():
+    data = shopping_list.export_shopping_list(os.environ.get("BASE_URL"), os.environ.get("API_KEY"))
+    return Response(data, mimetype="text/plain", headers={"Content-Disposition": "attachment;filename=shopping_list.csv"})
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- shopping_list.py
+++ shopping_list.py
@@ -1,65 +1,60 @@
 import grequests
-from dotenv import load_dotenv
-import os
 
-load_dotenv()
 
-BASE_URL = os.environ.get("BASE_URL")
-headers = {
-    "GROCY-API-KEY": os.environ.get("API_KEY")
-}
+def export_shopping_list(base_url, api_key):
+    headers = {
+        "GROCY-API-KEY": api_key
+    }
 
-urls = [
-    "/objects/shopping_list",
-    "/objects/quantity_units",
-    "/objects/products",
-    "/objects/product_groups"
-]
+    urls = [
+        "/objects/shopping_list",
+        "/objects/quantity_units",
+        "/objects/products",
+        "/objects/product_groups"
+    ]
 
-rs = (grequests.get(f"{BASE_URL}{u}", headers=headers) for u in urls)
-map = grequests.map(rs)
+    rs = (grequests.get(f"{base_url}{u}", headers=headers) for u in urls)
+    map = grequests.map(rs)
 
-shopping_list = map[0].json()
-units = map[1].json()
-products = map[2].json()
-product_groups = map[3].json()
+    shopping_list = map[0].json()
+    units = map[1].json()
+    products = map[2].json()
+    product_groups = map[3].json()
 
-parsed_units = {}
-for item in units:
-    parsed_units[item["id"]] = item["name"]
+    parsed_units = {}
+    for item in units:
+        parsed_units[item["id"]] = item["name"]
 
-parsed_products = {}
-for item in products:
-    parsed_products[item["id"]] = {"name": item["name"], "product_group_id": item["product_group_id"],
-                                   "conversion_rate": item["qu_factor_purchase_to_stock"],
-                                   "qu_id_purchase": item["qu_id_purchase"]}
+    parsed_products = {}
+    for item in products:
+        parsed_products[item["id"]] = {"name": item["name"], "product_group_id": item["product_group_id"],
+                                       "conversion_rate": item["qu_factor_purchase_to_stock"],
+                                       "qu_id_purchase": item["qu_id_purchase"]}
 
-parsed_product_groups = {}
-for item in product_groups:
-    parsed_product_groups[item["id"]] = item["name"]
+    parsed_product_groups = {}
+    for item in product_groups:
+        parsed_product_groups[item["id"]] = item["name"]
 
-compressed_list = {}
-for item in shopping_list:
-    product_id = item["product_id"]
-    if product_id in compressed_list:
-        compressed_list[product_id]["amount"] = compressed_list[product_id]["amount"] + float(item["amount"])
-    else:
-        compressed_list[product_id] = {"amount": float(item["amount"]), "unit": item["qu_id"]}
+    compressed_list = {}
+    for item in shopping_list:
+        product_id = item["product_id"]
+        if product_id in compressed_list:
+            compressed_list[product_id]["amount"] = compressed_list[product_id]["amount"] + float(item["amount"])
+        else:
+            compressed_list[product_id] = {"amount": float(item["amount"]), "unit": item["qu_id"]}
 
-pretty_list = {}
-for item in compressed_list.items():
-    product = parsed_products[item[0]]
-    name = product["name"].replace("*", "")
-    product_group = parsed_product_groups[product["product_group_id"]]
-    amount = item[1]["amount"] / float(product["conversion_rate"])
-    unit = parsed_units[product["qu_id_purchase"]]
+    pretty_list = {}
+    for item in compressed_list.items():
+        product = parsed_products[item[0]]
+        name = product["name"].replace("*", "")
+        product_group = parsed_product_groups[product["product_group_id"]]
+        amount = item[1]["amount"] / float(product["conversion_rate"])
+        unit = parsed_units[product["qu_id_purchase"]]
 
-    pretty_list[name] = {"amount": amount, "unit": unit, "product_group": product_group}
+        pretty_list[name] = {"amount": amount, "unit": unit, "product_group": product_group}
 
-data = ""
-for item in pretty_list.items():
-    data = data + item[0] + "," + str(round(item[1]["amount"])) + "," + item[1]["unit"] + "," + item[1]["product_group"] + "\n"
+    data = ""
+    for item in pretty_list.items():
+        data = data + item[0] + "," + str(round(item[1]["amount"])) + "," + item[1]["unit"] + "," + item[1]["product_group"] + "\n"
 
-with open("shopping_list.csv", "w") as f:
-    f.write(data)
-    f.close()
+    return data
