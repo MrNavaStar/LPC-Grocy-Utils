f07a4ab5675860fb17a54f5d71772ec72f363578
MrNavaStar <Mr.NavaStar@gmail.com>
2022-08-02 17:35:55 -0600

    backend for creating inventory templates

---
+++ inventory.py
@@ -0,0 +1,26 @@
+from requests import get
+
+
+def export_template(base_url, api_key):
+    headers = {
+        "GROCY-API-KEY": api_key,
+        "Content-Type": "application/json"
+    }
+
+    products = get(f"{base_url}/objects/products", headers=headers).json()
+    product_groups = get(f"{base_url}/objects/product_groups", headers=headers).json()
+
+    parsed_products = {}
+    for item in products:
+        parsed_products[item["id"]] = {"name": item["name"], "product_group_id": item["product_group_id"]}
+
+    parsed_product_groups = {}
+    for item in product_groups:
+        parsed_product_groups[item["id"]] = item["name"]
+
+    data = ""
+    for product in parsed_products.values():
+        data += product["name"].replace("*", "") + "\n"
+
+    return data
+
--- router.py
+++ router.py
@@ -1,6 +1,8 @@
 import os
 from datetime import date
 from flask import Flask, render_template, Response, request, redirect
+
+import inventory
 import shopping_list
 from dotenv import load_dotenv
 
@@ -20,17 +22,27 @@ def export_shopping_list():
     form_data = request.form["store_id"].split(":")
     data = shopping_list.export_shopping_list(BASE_URL, API_KEY, form_data[0])
     d = str(date.today())
-    return Response(data, mimetype="text/plain", headers={"Content-Disposition": f"attachment;filename={form_data[1]}_shopping_list_" + d + ".csv"})
+    return Response(data, mimetype="text/plain",
+                    headers={"Content-Disposition": f"attachment;filename={form_data[1]}_shopping_list_" + d + ".csv"})
 
 
 @app.route("/api/add_meal_plan_to_shopping_list", methods=['POST'])
 def add_meal_plan_to_shopping_list_raw():
     form_data = request.form
-    result = shopping_list.add_meal_plan_to_shopping_list(BASE_URL, API_KEY, form_data["start-date"], form_data["end-date"])
+    result = shopping_list.add_meal_plan_to_shopping_list(BASE_URL, API_KEY, form_data["start-date"],
+                                                          form_data["end-date"])
 
     if result == "Bad Date":
         return "You Must Enter a Valid Date"
     return redirect("/")
+
+
+@app.route("/api/export_inventory_template")
+def export_inventory_template():
+    data = inventory.export_template(BASE_URL, API_KEY)
+
+    return Response(data, mimetype="text/plain",
+                    headers={"Content-Disposition": f"attachment;filename=inventory_template.csv"})
 
 
 if __name__ == '__main__':
